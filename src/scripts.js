3a9b3e0f2c1c95d1053dec46530128dbe614ad63
David Swatzell <thatsstrikethree@gmail.com>
2024-04-20 12:11:13 -0500

    add imports of api functions into scripts js

--- src/scripts.js
+++ src/scripts.js
@@ -4,9 +4,15 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.scss';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-
+import {
+    getAllCustomers,
+    getCustomerById,
+    getAllRooms,
+    getAllBookings,
+    addBooking,
+    deleteBookingById,
+  } from './apiCalls.js';
+  
 
 console.log('This is the JavaScript entry file - your code begins here.');
 const loginPage = document.querySelector('.login-page');
